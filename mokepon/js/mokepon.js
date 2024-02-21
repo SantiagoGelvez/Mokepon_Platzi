c3ca81032109afe71a7f03e3e953415e8e81e3f0
Santiago Gelvez <santiagogelvez29@gmail.com>
2024-02-20 20:43:37 -0500

    Mokepon: Add logic for random enemy selection

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -19,6 +19,25 @@ function selectPet() {
     } else {
         alert("Select a pet")
     }
+
+    selectEnemyPet()
+}
+
+function selectEnemyPet() {
+    let randomAttack = randomChoice(1, 3)
+    let spanEnemyPet = document.getElementById("enemy-pet")
+    
+    if (randomAttack == 1) {
+        spanEnemyPet.innerHTML = "Hipodoge"
+    } else if (randomAttack == 2) {
+        spanEnemyPet.innerHTML = "Capipepo"
+    } else {
+        spanEnemyPet.innerHTML = "Ratigueya"
+    }
+}
+
+function randomChoice(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener("load", initGame)
