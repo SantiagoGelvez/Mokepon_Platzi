46230cb047b49356e550dedab764f4fbbf9b45f4
Santiago Gelvez <santiagogelvez29@gmail.com>
2024-02-20 19:49:00 -0500

    Mokepon: Organize html indent and add logic for pet selection

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,21 @@
-function selectPet() {
-    alert("You select a pet")
+function initGame() {
+    let buttonPet = document.getElementById("button-pet")
+    buttonPet.addEventListener("click", selectPet)
 }
 
-let buttonPet = document.getElementById("button-pet")
-buttonPet.addEventListener("click", selectPet)
+function selectPet() {
+    hipodoge = document.getElementById('hipodoge')
+    capipepo = document.getElementById('capipepo')
+    ratigueya = document.getElementById('ratigueya')
+    if (hipodoge.checked) {
+        alert("You select hipodoge")
+    } else if (capipepo.checked) {
+        alert("You select capipepo")
+    } else if (ratigueya.checked) {
+        alert("You select ratigueya")
+    } else {
+        alert("Pet no selected yet")
+    }
+}
+
+window.addEventListener("load", initGame)
